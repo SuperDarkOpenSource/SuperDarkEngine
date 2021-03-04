61228f5dd18db9029cf02d947938b6ecd91a0e4f
russell.johnson <encrypteddata0@gmail.com>
2021-03-03 16:37:08 -0800

    Added base GameEngine and FFI code

--- Engine/Engine/src/core/game_engine.rs
+++ Engine/Engine/src/core/game_engine.rs
@@ -1,4 +1,4 @@
-use crate::core::window::Window;
+use crate::core::window::{Window, NullWindow};
 
 pub struct GameEngineCreationInfo
 {
@@ -6,8 +6,46 @@ pub struct GameEngineCreationInfo
 
 }
 
+impl GameEngineCreationInfo {
+    pub fn new() -> Self {
+        GameEngineCreationInfo {
+            window: Box::new(NullWindow{})
+        }
+    }
+}
+
 
 pub struct GameEngine
 {
+    create_info: Box<GameEngineCreationInfo>,
+    do_next_frame: bool,
+}
 
+impl GameEngine {
+
+    pub fn new(create_info: Box<GameEngineCreationInfo>) -> Self {
+        let mut engine = GameEngine {
+            create_info,
+            do_next_frame: true
+        };
+
+        //engine.create_info.window.register_msg_handler("hello",  self::set_run_false);
+
+        engine
+    }
+
+    pub fn run(&mut self) {
+
+        let mut window = &mut self.create_info.window;
+
+        loop {
+            window.update();
+        }
+    }
+
+    pub fn set_run_false(&mut self, _msg: &str) -> bool {
+        self.do_next_frame = false;
+
+        true
+    }
 }
--- Engine/Engine/src/core/window.rs
+++ Engine/Engine/src/core/window.rs
@@ -1,9 +1,15 @@
 
+use std::ffi::{CStr, CString};
+use std::ptr::null;
+use serde_json::Value;
+use std::iter::Map;
+use std::collections::HashMap;
+
 pub type WindowMsgHandler = fn (&str) -> bool;
 
 pub type ExternalWindowUpdateFn = fn();
-pub type ExternalWindowReceiveMsgFn = fn(*const u8);
-pub type ExternalWindowDeliverMsgFn = fn(*const u8);
+pub type ExternalWindowReceiveMsgFn = fn() -> *const i8;
+pub type ExternalWindowDeliverMsgFn = fn(*const i8);
 
 pub trait Window
 {
@@ -28,7 +34,7 @@ impl Window for NullWindow
 
     }
 
-    fn register_msg_handler(&mut self, _command: &str, _handler: fn(&str) -> bool) {
+    fn register_msg_handler(&mut self, _command: &str, _handler: WindowMsgHandler) {
 
     }
 }
@@ -38,6 +44,7 @@ pub struct ExternalWindow
     pub update_fn: ExternalWindowUpdateFn,
     pub receive_fn: ExternalWindowReceiveMsgFn,
     pub deliver_fn: ExternalWindowDeliverMsgFn,
+    command_map: HashMap<String, WindowMsgHandler>
 }
 
 impl ExternalWindow
@@ -47,8 +54,43 @@ impl ExternalWindow
         ExternalWindow
         {
             update_fn: external_update_fn_stub,
-            receive_fn: external_u8_fn_stub,
-            deliver_fn: external_u8_fn_stub
+            receive_fn: external_i8_receive_stub,
+            deliver_fn: external_i8_fn_stub,
+            command_map: HashMap::new(),
+        }
+    }
+
+    fn poll_external_msg_queue(&self) {
+        loop {
+            let msg_raw: *const i8 = (&self.receive_fn)();
+
+            if msg_raw == null() {
+                break;
+            }
+
+            let msg = unsafe{CStr::from_ptr(msg_raw)};
+
+            let json_msg: Value = serde_json::from_str(msg.to_str().unwrap()).unwrap();
+
+            match json_msg {
+                Value::Object(value) => {
+                    if value.contains_key("command") && value.contains_key("payload") {
+                        let command = value["command"].as_str().unwrap();
+                        let payload = value["payload"].as_str().unwrap();
+
+                        self.handle_received_msg(command, payload);
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+
+    fn handle_received_msg(&self, command: &str, payload: &str) {
+        if self.command_map.contains_key(command) {
+            (&self.command_map[command])(payload);
+        } else {
+            println!("Command {} not found in ExternalWindow command handlers", command);
         }
     }
 }
@@ -56,11 +98,17 @@ impl ExternalWindow
 impl Window for ExternalWindow
 {
     fn update(&mut self) {
-        (self.update_fn)();
+        (&self.update_fn)();
+
+        self.poll_external_msg_queue();
     }
 
     fn send_msg(&mut self, msg: &str) {
-        unimplemented!()
+        // Convert to a C String
+        let cstr = CString::new(msg).unwrap();
+
+        // FFI to the ExternalWindow
+        (&self.deliver_fn)(cstr.as_ptr());
     }
 
     fn register_msg_handler(&mut self, command: &str, handler: fn(&str) -> bool) {
@@ -68,10 +116,11 @@ impl Window for ExternalWindow
     }
 }
 
-fn external_update_fn_stub()
-{
+fn external_update_fn_stub() {
 }
 
-fn external_u8_fn_stub(_: *const u8)
-{
+fn external_i8_fn_stub(_: *const i8) { }
+
+fn external_i8_receive_stub() -> *const i8 {
+    null()
 }
